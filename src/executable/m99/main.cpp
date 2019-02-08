c43095007f1009388ba78a09582eaa5462bc1a51
Michael Maniscalco <mmaniscalco@limebrokerage.com>
2019-02-08 17:11:23 -0500

    added option for blocksize

--- src/executable/m99/main.cpp
+++ src/executable/m99/main.cpp
@@ -46,21 +46,12 @@ namespace
     (
         std::uint8_t const * begin,
         std::uint8_t const * end,
-        char const * outputPath,
+        std::ofstream & outStream,
         std::uint32_t numThreads
     )
     {        
         std::uint32_t inputSize = std::distance(begin, end);
 
-        // create the output stream
-        std::ofstream outStream(outputPath, std::ios_base::out | std::ios_base::binary);
-        if (!outStream.is_open())
-        {
-            std::cout << "failed to create output file \"" << outputPath << "\"" << std::endl;
-            return;
-        }
-
-        auto startTime = std::chrono::system_clock::now();
         auto sentinelIndex = maniscalco::forward_burrows_wheeler_transform(begin, end, numThreads);
 
 #ifdef ENABLE_EXPERIMENTAL
@@ -86,8 +77,6 @@ namespace
         for (auto & e : encodingThreads)
             e.join();
 
-        auto finishTime = std::chrono::system_clock::now();
-        auto elapsedOverallEncode = std::chrono::duration_cast<std::chrono::milliseconds>(finishTime - startTime).count();
 
         std::size_t outputSize = 0;
         outputSize = (sizeof(inputSize) + sizeof(sentinelIndex) + sizeof(bytesPerBlock));
@@ -96,8 +85,14 @@ namespace
             outputSize += e.size();
             outputSize += sizeof(std::uint32_t);  
         }
-        std::cout << "compressed: " << inputSize << " -> " << outputSize << " bytes.  ratio = " << (((long double)outputSize / inputSize) * 100) << "%" << std::endl;
-        std::cout << "Elapsed time: " << ((long double)elapsedOverallEncode / 1000) << " seconds : " <<  (((long double)inputSize / (1 << 20)) / ((double)elapsedOverallEncode / 1000)) << " MB/sec" << std::endl;
+
+        std::uint32_t totalEncodedSize = sizeof(inputSize) + sizeof(sentinelIndex) + sizeof(bytesPerBlock);
+        for (auto const & e : encodeBuffers)
+        {
+            totalEncodedSize += e.size();
+            totalEncodedSize += sizeof(std::uint32_t);
+        }
+        outStream.write((char *)&totalEncodedSize, sizeof(totalEncodedSize));
 
         outStream.write((char *)&inputSize, sizeof(inputSize));
         outStream.write((char *)&sentinelIndex, sizeof(sentinelIndex));
@@ -108,7 +103,6 @@ namespace
             outStream.write((char const *)&encodedSize, sizeof(encodedSize));
             outStream.write((char const *)&*e.begin(), encodedSize);
         }
-        outStream.close();
     }
 
 
@@ -116,7 +110,7 @@ namespace
     void decode
     (
         std::uint8_t const * begin,
-        char const * outputPath,
+        std::ofstream & outStream,
         std::uint32_t numThreads
     )
     {
@@ -132,7 +126,7 @@ namespace
         auto inputCurrent = begin;
         auto outputCurrent = decodedData.data();
         auto bytesToDecode = originalSize;
-        auto startTime = std::chrono::system_clock::now();
+
 
         std::array<int32_t, 256> symbolCount;
         for (auto & e : symbolCount)
@@ -162,20 +156,10 @@ namespace
                 e.join();
         }
         maniscalco::reverse_burrows_wheeler_transform(decodedData.begin(), decodedData.end(), sentinelIndex, numThreads);
-
-        auto finishTime = std::chrono::system_clock::now();
-        auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(finishTime - startTime).count();
-        std::cout << "Elapsed time: " << ((long double)elapsedTime / 1000) << " seconds : " <<  (((long double)originalSize / (1 << 20)) / ((double)elapsedTime / 1000)) << " MB/sec" << std::endl;
         
-        // create the output stream
-        std::ofstream outStream(outputPath, std::ios_base::out | std::ios_base::binary);
-        if (!outStream.is_open())
-        {
-            std::cout << "failed to create output file \"" << outputPath << "\"" << std::endl;
-            return;
-        }
+
         outStream.write((char const *)&*decodedData.begin(), decodedData.size());
-        outStream.close();
+     //   outStream.close();
     }
 
 
@@ -193,8 +177,116 @@ namespace
     (
     )
     {
-        std::cout << "Usage: m99 [e|d] inputFile outputFile threadCount" << std::endl;
+        std::cout << "Usage: m99 [e|d] inputFile outputFile [switches]" << std::endl;
+        std::cout << "\t -t = threadCount" << std::endl;
+        std::cout << "\t -b = blockSize (max = 1GB)" << std::endl; 
+
+        std::cout << "example: m99 e inputFile outputFile -t8 -b100000" << std::endl;
+        std::cout << "example: m99 d inputFile outputFile -t8" << std::endl; 
         return 0;
+    }
+
+
+    //==========================================================================
+    void decode
+    (
+        char const * inputPath,
+        char const * outputPath,
+        int numThreads
+    )
+    {
+        // read data from file
+        std::ifstream inputStream(inputPath, std::ios_base::in | std::ios_base::binary);
+        if (!inputStream.is_open())
+        {
+            std::cout << "failed to open file \"" << inputPath << "\"" << std::endl;
+            return;
+        }
+
+        // create the output stream
+        std::ofstream outStream(outputPath, std::ios_base::out | std::ios_base::binary);
+        if (!outStream.is_open())
+        {
+            std::cout << "failed to create output file \"" << outputPath << "\"" << std::endl;
+            return;
+        }
+
+        std::vector<std::uint8_t> input;
+        input.reserve(1 << 30);
+
+        auto startTime = std::chrono::system_clock::now();
+        while (true)
+        {
+            std::uint32_t blockSize = 0;
+            inputStream.read((char *)&blockSize, sizeof(blockSize));
+            if (inputStream.gcount() == 0)
+                break;
+            input.resize(blockSize);
+            inputStream.read((char *)input.data(), blockSize);
+            decode(input.data(), outStream, numThreads);
+        }
+
+        auto finishTime = std::chrono::system_clock::now();
+        auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(finishTime - startTime).count();
+        std::cout << "Elapsed time: " << ((long double)elapsedTime / 1000) << " seconds" << std::endl;
+
+        inputStream.close();
+        outStream.close();
+    }
+
+
+    //=================================================================================
+    void encode
+    (
+        char const * inputPath,
+        char const * outputPath,
+        int numThreads,
+        int blockSize
+    )
+    {
+        // create the output stream
+        std::ofstream outStream(outputPath, std::ios_base::out | std::ios_base::binary);
+        if (!outStream.is_open())
+        {
+            std::cout << "failed to create output file \"" << outputPath << "\"" << std::endl;
+            return;
+        }
+
+        auto startTime = std::chrono::system_clock::now();
+
+        // read data from file
+        std::vector<std::uint8_t> input;
+        input.reserve(blockSize);
+        std::ifstream inputStream(inputPath, std::ios_base::in | std::ios_base::binary);
+        if (!inputStream.is_open())
+        {
+            std::cout << "failed to open file \"" << inputPath << "\"" << std::endl;
+            return;
+        }
+
+        std::size_t bytesEncoded = 0;
+        inputStream.seekg(0, std::ios_base::beg);
+        while (true)
+        {
+            inputStream.read((char *)input.data(), input.capacity());
+            auto size = inputStream.gcount();
+            if (size == 0)
+                break;
+            bytesEncoded += size;
+            encode(input.data(), input.data() + size, outStream, numThreads);
+        }
+
+        auto finishTime = std::chrono::system_clock::now();
+        auto elapsedOverallEncode = std::chrono::duration_cast<std::chrono::milliseconds>(finishTime - startTime).count();
+
+        std::size_t inputSize = bytesEncoded;
+        std::size_t outputSize = outStream.tellp();
+
+        std::cout << "compressed: " << inputSize << " -> " << outputSize << " bytes.  ratio = " << (((long double)outputSize / inputSize) * 100) << "%" << std::endl;
+        std::cout << "Elapsed time: " << ((long double)elapsedOverallEncode / 1000) << " seconds : " <<  (((long double)inputSize / (1 << 20)) / ((double)elapsedOverallEncode / 1000)) << " MB/sec" << std::endl;
+
+        outStream.close();
+        inputStream.close();
     }
 }
 
@@ -212,22 +304,59 @@ std::int32_t main
         return print_usage();
 
     std::size_t numThreads = 0;
-    if (argCount == 5)
+    std::size_t maxBlockSize = (1 << 30);
+    for (auto argIndex = 4; argIndex < argCount; ++argIndex)
     {
-        // optional args added
-        numThreads = 0;
-        auto cur = argValue[4];
-        while (*cur != 0)
+        if (argValue[argIndex][0] != '-')
+            return print_usage();
+
+        switch (argValue[argIndex][1])
         {
-            if ((*cur < '0') || (*cur > '9'))
+            case 'b':
             {
-                std::cout << "invalid thread count" << std::endl;
-                print_usage();
-                return -1;
+                // block size
+                maxBlockSize = 0;
+                auto cur = argValue[argIndex] + 2;
+                while (*cur != 0)
+                {
+                    if ((*cur < '0') || (*cur > '9'))
+                    {
+                        std::cout << "invalid block size" << std::endl;
+                        print_usage();
+                        return -1;
+                    }
+                    maxBlockSize *= 10;
+                    maxBlockSize += (*cur - '0');
+                    ++cur;
+                }
+                if (maxBlockSize > (1 << 30))
+                    maxBlockSize = (1 << 30);
+                break;
             }
-            numThreads *= 10;
-            numThreads += (*cur - '0');
-            ++cur;
+            case 't':
+            {
+                // thread count
+                numThreads = 0;
+                auto cur = argValue[argIndex] + 2;
+                while (*cur != 0)
+                {
+                    if ((*cur < '0') || (*cur > '9'))
+                    {
+                        std::cout << "invalid thread count" << std::endl;
+                        print_usage();
+                        return -1;
+                    }
+                    numThreads *= 10;
+                    numThreads += (*cur - '0');
+                    ++cur;
+                }
+                break;
+            }
+            default:
+            {
+                std::cout << "unknown switch: " << argValue[argIndex] << std::endl;
+                return print_usage();
+            }
         }
     }
     if ((numThreads == 0))// || (numThreads > std::thread::hardware_concurrency()))
@@ -237,15 +366,17 @@ std::int32_t main
     {
         case 'e':
         {
-            auto input = load_file(argValue[2]);
-            encode((std::uint8_t const *)input.data(), (std::uint8_t const *)input.data() + input.size(), argValue[3], numThreads);
+            encode(argValue[2], argValue[3], numThreads, maxBlockSize);
+//            auto input = load_file(argValue[2]);
+  //          encode((std::uint8_t const *)input.data(), (std::uint8_t const *)input.data() + input.size(), argValue[3], numThreads);
             break;
         }
 
         case 'd':
         {
-            auto input = load_file(argValue[2]);
-            decode((std::uint8_t const *)input.data(), argValue[3], numThreads);
+            decode(argValue[2], argValue[3], numThreads);
+//            auto input = load_file(argValue[2]);
+  //          decode((std::uint8_t const *)input.data(), argValue[3], numThreads);
             break;
         }
 
